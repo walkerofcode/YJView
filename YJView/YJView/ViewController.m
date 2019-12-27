f66fc285eba0b1917a69e38b5a5db1d77daff2e0
杨建 <alen20160902@gmail.com>
2019-12-27 16:32:16 +0800

    提交

--- YJView/YJView/ViewController.m
+++ YJView/YJView/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "YJView.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) YJView* yjView;
 @end
 
 @implementation ViewController
@@ -17,6 +18,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    YJView* yjView = [[YJView alloc] initWithFrame:self.view.bounds configuration:nil];
+    NSURL* url = [NSURL URLWithString:@"http://www.baidu.com"];
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    [yjView loadRequest:request];
+    self.yjView = yjView;
+    
+    [self.view addSubview:yjView];
 }
 
 
